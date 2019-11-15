1fbab23cbfa0c69718ac5848ffa82c23806eb9df
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2019-11-15 12:14:35 +0100

    Adding time intent

--- SampleApplication.py
+++ SampleApplication.py
@@ -12,7 +12,7 @@ class SampleApplication(Base.AbstractApplication):
 
         # Pass the required Dialogflow parameters (add your Dialogflow parameters)
         self.setDialogflowKey('nao_key.json')
-        self.setDialogflowAgent('NAO')
+        self.setDialogflowAgent('nao-akwxxe')
 
         # Make the robot ask the question, and wait until it is done speaking
         self.speechLock = Semaphore(0)
@@ -24,7 +24,7 @@ class SampleApplication(Base.AbstractApplication):
         self.nameLock = Semaphore(0)
         self.setAudioContext('answer_name')
         self.startListening()
-        self.nameLock.acquire(timeout=3)
+        self.nameLock.acquire(timeout=5)
         self.stopListening()
         if not self.name:  # wait one more second after stopListening (if needed)
             self.nameLock.acquire(timeout=1)
@@ -35,6 +35,25 @@ class SampleApplication(Base.AbstractApplication):
         else:
             self.sayAnimated('Sorry, I didn\'t catch your name.')
         self.speechLock.acquire()
+
+        # Ask for the time
+        self.speechLock = Semaphore(0)
+        self.sayAnimated('Could you tell me the time ' + self.name + '?')
+        self.speechLock.acquire()
+
+        self.time = None
+        self.timelock = Semaphore(0)
+        self.setAudioContext('answer_time')
+        self.startListening()
+        self.timelock.acquire(timeout=5)
+        self.stopListening()
+
+        if self.time:
+            self.sayAnimated('So the time is  ' + self.time + '!')
+        else:
+            self.sayAnimated('Sorry, I didn\'t catch what you said.')
+        self.speechLock.acquire()
+
 
     def onRobotEvent(self, event):
         if event == 'LanguageChanged':
@@ -48,6 +67,9 @@ class SampleApplication(Base.AbstractApplication):
         if intentName == 'answer_name' and len(args) > 0:
             self.name = args[0]
             self.nameLock.release()
+        if intentName == 'answer_time' and len(args) > 0:
+            self.time = args[0]
+            self.timelock.release()
 
 # Run the application
 sample = SampleApplication()
