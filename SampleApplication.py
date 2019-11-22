52539260a16c18d72e5a307bd19c40ce70d06987
alfjesus3 <55023404+alfjesus3@users.noreply.github.com>
2019-11-22 12:24:48 +0100

    Update conversation

--- SampleApplication.py
+++ SampleApplication.py
@@ -1,9 +1,74 @@
 import AbstractApplication as Base
+import numpy as np
 from time import sleep
 from threading import Semaphore
 
 
 class SampleApplication(Base.AbstractApplication):
+    def general_repeat_Interaction(self):
+        # Ask patient to repeat
+        self.sayAnimated('Could you repeat, please?!')
+        self.speechLock.acquire()
+
+    def feeling_in_general(self):
+        # Ask how the patient is feeling
+        self.speechLock = Semaphore(0)
+        self.sayAnimated('How are you feeling today?')
+        self.speechLock.acquire()
+
+        loop = 0
+        output = False
+        while loop < 2:
+            self.emotion = None
+            self.emotionLock = Semaphore(0)
+            self.setAudioContext('answer_how_you_feeling')
+            self.startListening()
+            self.emotionLock.acquire(timeout=5)
+            self.stopListening()
+            if not self.emotion:  # wait one more second after stopListening (if needed)
+                self.emotionLock.acquire(timeout=1)
+
+            if self.emotion:
+                loop = 2
+                output = True
+                if self.emotion == 'happy':
+                    self.say('So you are feelin ' + self.emotion)
+                    self.doGesture('happy/behavior_1') #custom animation installed on nao
+                else:
+                    self.sayAnimated('Too bad dude.')
+            else:
+                self.sayAnimated('Sorry, I didn\'t catch what you said.')
+                loop += 1
+                general_repeat_Interaction()
+        self.speechLock.acquire()
+
+    def feeling_about_meal_interaction(self):
+        # Ask how the patient is feeling
+        self.speechLock = Semaphore(0)
+        self.sayAnimated('How did you feel after your last meal?')
+        self.speechLock.acquire()
+
+        self.mealEmotion = None
+        self.emotionLock = Semaphore(0)
+        self.setAudioContext('answer_how_you_feeling_after_meal')
+        self.startListening()
+        self.emotionLock.acquire(timeout=5)
+        self.stopListening()
+        if not self.mealEmotion:  # wait one more second after stopListening (if needed)
+            self.emotionLock.acquire(timeout=1)
+
+        if self.mealEmotion:
+            if self.mealEmotion == 'good_feeling':
+                self.say('So you are feelin ' + self.mealEmotion)
+                self.doGesture('happy/behavior_1') #custom animation installed on nao
+                self.sayAnimated(compliments[np.random.randint(0, len(compliments))])
+            else:
+                self.sayAnimated(quotes[np.random.randint(0, len(quotes))])
+        else:
+            self.sayAnimated('Sorry, I didn\'t catch what you said.')
+        self.speechLock.acquire()
+
+
     def main(self):
         # Set the correct language (and wait for it to be changed)
         self.langLock = Semaphore(0)
@@ -56,29 +121,9 @@ class SampleApplication(Base.AbstractApplication):
         #     self.sayAnimated('Sorry, I didn\'t catch what you said.')
         # self.speechLock.acquire()
 
-        # Ask how the patient is feeling
-        self.speechLock = Semaphore(0)
-        self.sayAnimated('How are you feeling today?')
-        self.speechLock.acquire()
+        feeling_in_general()
 
-        self.emotion = None
-        self.emotionLock = Semaphore(0)
-        self.setAudioContext('answer_how_you_feeling')
-        self.startListening()
-        self.emotionLock.acquire(timeout=5)
-        self.stopListening()
-        if not self.emotion:  # wait one more second after stopListening (if needed)
-            self.emotionLock.acquire(timeout=1)
-
-        if self.emotion:
-            if self.emotion == 'happy':
-                self.say('So you are feelin ' + self.emotion)
-                self.doGesture('happy/behavior_1') #custom animation installed on nao
-            else:
-                self.sayAnimated('Too bad dude.')
-        else:
-            self.sayAnimated('Sorry, I didn\'t catch what you said.')
-        self.speechLock.acquire()
+        feelings_about_meal_interaction()
 
     def onRobotEvent(self, event):
         if event == 'LanguageChanged':
