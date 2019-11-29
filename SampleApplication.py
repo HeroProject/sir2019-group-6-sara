cc42109dbf64dd52b833cbb43393fbaff2552454
Etienne <egalea.11@gmail.com>
2019-11-29 11:49:02 +0100

    AudioIntents are set generic

--- SampleApplication.py
+++ SampleApplication.py
@@ -16,11 +16,11 @@ class SampleApplication(Base.AbstractApplication):
         self.setDialogflowAgent('nao-akwxxe')
 
         # Asking the patient how they are feeling
-        self.interaction('how are you feeling today?',
-                         'answer_how_you_feeling',
-                         ['happy'],
-                         ['So you are feeling', 'I\'m sorry to hear that'],
-                         ['happy/behavior_1'])
+        # self.interaction('how are you feeling today?',
+        #                  'answer_how_you_feeling',
+        #                  ['happy'],
+        #                  ['So you are feeling', 'I\'m sorry to hear that'],
+        #                  ['happy/behavior_1'])
 
         # Asking the patient how they felt after their last meal
         self.interaction('How did you feel after your last meal?',
@@ -32,30 +32,30 @@ class SampleApplication(Base.AbstractApplication):
     def interaction(self, question, intent, entities, responseText, gesture, listeningTimeout=5, repeatMax=2):
         # Ask how the patient is feeling
         self.nao_speech(question)
-        self.general_repeat_Interaction(intent, entities, responseText, gesture, listeningTimeout=5, repeatMax=2)
+        self.general_repeat_Interaction(intent, entities, responseText, gesture, listeningTimeout, repeatMax)
 
     def general_repeat_Interaction(self, intent, entities, responseText, gesture, listeningTimeout=5, repeatMax=2):
         for i in range(0, repeatMax):
             # init emotion
-            self.emotion = None
-            self.emotionLock = Semaphore(0)
+            self.intentInfo = None
+            self.interactionLock = Semaphore(0)
 
             # setting intent to listen to
             self.setAudioContext(intent)
 
             # listen to answer
             self.startListening()
-            self.emotionLock.acquire(timeout=listeningTimeout)
+            self.interactionLock.acquire(timeout=listeningTimeout)
             self.stopListening()
             # if emotion is still not set, wait some more
-            if not self.emotion:
-                self.emotionLock.acquire(timeout=1)
+            if not self.intentInfo:
+                self.interactionLock.acquire(timeout=1)
 
             # emotion is set
-            if self.emotion:
+            if self.intentInfo:
                 output = True
-                if self.emotion == entities[0]:
-                    self.say(responseText[0] + self.emotion)
+                if self.intentInfo == entities[0]:
+                    self.say(responseText[0] + self.intentInfo)
                     # perform custom animation gesture installed on nao (from choreograph)
                     self.doGesture(gesture[0])
                 else:
@@ -63,8 +63,9 @@ class SampleApplication(Base.AbstractApplication):
                 return False
             else:
                 # Ask patient to repeat
-                self.nao_speech()
-                self.setAudioContext(intent)
+                if (i != repeatMax-1):
+                    self.nao_speech()
+                    self.setAudioContext(intent)
 
     # Default speech is 'repeat please'
     def nao_speech(self, speech='Could you repeat, please?!'):
@@ -81,15 +82,9 @@ class SampleApplication(Base.AbstractApplication):
             self.gestureLock.release()
 
     def onAudioIntent(self, *args, intentName):
-        if intentName == 'answer_name' and len(args) > 0:
-            self.name = args[0]
-            self.nameLock.release()
-        if intentName == 'answer_time' and len(args) > 0:
-            self.time = args[0]
-            self.timelock.release()
-        if intentName == 'answer_how_you_feeling' and len(args) > 0:
-            self.emotion = args[0]
-            self.emotionLock.release()
+        if len(args) > 0:
+            self.intentInfo = args[0]
+            self.interactionLock.release()
 
 
 # Run the application
