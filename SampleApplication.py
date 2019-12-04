359a88f6d7724d950029d4fc703938fe0f778d87
Etienne <egalea.11@gmail.com>
2019-12-04 16:15:52 +0100

    Fixed indentation

--- SampleApplication.py
+++ SampleApplication.py
@@ -21,7 +21,7 @@ class SampleApplication(Base.AbstractApplication):
                          'answer_how_you_feeling',
                          ['happy'],
                          ['So you are feeling', 'I\'m sorry to hear that'],
-                         feeling_reaction,
+                         self.feeling_reaction,
                          ['happy/behavior_1'])
 
         # Asking the patient how they felt after their last meal
@@ -29,7 +29,7 @@ class SampleApplication(Base.AbstractApplication):
                          'answer_how_you_feeling_after_meal',
                          ['good_feeling', 'bad_feeling'],
                          ['So you are feeling', 'I\'m sorry to hear that'],
-                         after_meal_reaction,
+                         self.after_meal_reaction,
                          ['happy/behavior_1'])
 
     def interaction(self, question, intent, entities, responseText, gesture, reaction_function, listeningTimeout=5,
@@ -69,78 +69,76 @@ class SampleApplication(Base.AbstractApplication):
                     self.nao_speech()
                     self.setAudioContext(intent)
 
+    def game(self):
+        self.nao_speech(speech="Let's play a game of rock paper scissors.")
 
-def game(self):
-    self.nao_speech(speech="Let's play a game of rock paper scissors.")
+        self.gestureLock = Semaphore(0)
+        self.doGesture('game/behavior_1')
+        self.gestureLock.acquire()
 
-    self.gestureLock = Semaphore(0)
-    self.doGesture('game/behavior_1')
-    self.gestureLock.acquire()
+        game = np.random.uniform(0, 3)
+        if game < 1:
+            self.say("Rock!")
+            self.doGesture('rock/rock')
+            self.played = "rock"
+        elif game < 2:
+            self.say("Paper!")
+            self.doGesture('paper/paper')
+            self.played = "paper"
+        else:
+            self.say("Scissors!")
+            self.doGesture('scissors/scissors')
+            self.played = "scissors"
 
-    game = np.random.uniform(0, 3)
-    if game < 1:
-        self.say("Rock!")
-        self.doGesture('rock/rock')
         self.played = "rock"
-    elif game < 2:
-        self.say("Paper!")
-        self.doGesture('paper/paper')
-        self.played = "paper"
-    else:
-        self.say("Scissors!")
-        self.doGesture('scissors/scissors')
-        self.played = "scissors"
-
-    self.played = "rock"
 
 
-def nao_speech(self, speech=['Could you repeat, please?!']):
-    sentance = ""
-    for phrase in speech:
-        sentance = sentance + phrase
+    def nao_speech(self, speech=['Could you repeat, please?!']):
+        sentance = ""
+        for phrase in speech:
+            sentance = sentance + phrase
 
-    self.speechLock = Semaphore(0)
-    self.sayAnimated(sentance)
-    self.speechLock.acquire()
-
-
-def feeling_reaction(self, entities, responseText, gesture):
-    if self.intentInfo == entities[0]:
-        self.say(responseText[0] + self.intentInfo)
-        self.doGesture(gesture[0])
-    else:
-        self.sayAnimated(responseText[1])
+        self.speechLock = Semaphore(0)
+        self.sayAnimated(sentance)
+        self.speechLock.acquire()
 
 
-def after_meal_reaction(self, entities, responseText, gesture):
-    if self.intentInfo == entities[0]:
-        self.say(responseText[0] + self.intentInfo)
-        self.sayAnimated(self.compliments[np.random.randint(0, len(self.compliments))])
-    else:
-        self.say(responseText[1])
-        self.sayAnimated(self.quotes[np.random.randint(0, len(self.quotes))])
+    def feeling_reaction(self, entities, responseText, gesture):
+        if self.intentInfo == entities[0]:
+            self.say(responseText[0] + self.intentInfo)
+            self.doGesture(gesture[0])
+        else:
+            self.sayAnimated(responseText[1])
 
 
-def nao_gesture(self, gesture):
-    self.gestureLock = Semaphore(0)
-    self.doGesture(gesture)
-    self.gestureLock.acquire()
+    def after_meal_reaction(self, entities, responseText, gesture):
+        if self.intentInfo == entities[0]:
+            self.say(responseText[0] + self.intentInfo)
+            self.sayAnimated(self.compliments[np.random.randint(0, len(self.compliments))])
+        else:
+            self.say(responseText[1])
+            self.sayAnimated(self.quotes[np.random.randint(0, len(self.quotes))])
 
 
-def onRobotEvent(self, event):
-    if event == 'LanguageChanged':
-        self.langLock.release()
-    elif event == 'TextDone':
-        self.speechLock.release()
-    elif event == 'GestureDone':
-        self.gestureLock.release()
+    def nao_gesture(self, gesture):
+        self.gestureLock = Semaphore(0)
+        self.doGesture(gesture)
+        self.gestureLock.acquire()
 
 
-def onAudioIntent(self, *args, intentName):
-    if len(args) > 0:
-        self.intentName = args[0]
-        self.interactionLock.release()
+    def onRobotEvent(self, event):
+        if event == 'LanguageChanged':
+            self.langLock.release()
+        elif event == 'TextDone':
+            self.speechLock.release()
+        elif event == 'GestureDone':
+            self.gestureLock.release()
 
+
+    def onAudioIntent(self, *args, intentInfo):
+        if len(args) > 0:
+            self.intentInfo = args[0]
+            self.interactionLock.release()
 
 # Run the application
 sample = SampleApplication()
